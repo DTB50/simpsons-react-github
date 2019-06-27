84b542c030540081fc1d01b7eefe66c5e3b91f99
DavidB <david.burns.dtb@googlemail.com>
2019-06-27 14:46:27 +0100

    Updates

--- precache-manifest.74902c37e85f6d0e42ac66ab403dccf7.js
+++ precache-manifest.74902c37e85f6d0e42ac66ab403dccf7.js
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/simpsons-react-github/static/js/runtime~main.b50981d8.js"
   },
   {
-    "revision": "f23011cd7fc30b2cdc30",
-    "url": "/simpsons-react-github/static/js/main.99c2a0ef.chunk.js"
+    "revision": "41e4edbc37cd85062515",
+    "url": "/simpsons-react-github/static/js/main.f334b3b9.chunk.js"
   },
   {
     "revision": "a4bfaf8c4bccdd0a2cf9",
     "url": "/simpsons-react-github/static/js/2.0771dcc1.chunk.js"
   },
   {
-    "revision": "f23011cd7fc30b2cdc30",
+    "revision": "41e4edbc37cd85062515",
     "url": "/simpsons-react-github/static/css/main.dbce3c70.chunk.css"
   },
   {
-    "revision": "014d0801c43764ca97ce62edffbaf648",
+    "revision": "e8fc68ff4b005d2f7abe0119ac35b9b6",
     "url": "/simpsons-react-github/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/simpsons-react-github/precache-manifest.f50064c7dea29404789162b5f4966a81.js"
+  "/simpsons-react-github/precache-manifest.74902c37e85f6d0e42ac66ab403dccf7.js"
 );
 
 workbox.clientsClaim();
